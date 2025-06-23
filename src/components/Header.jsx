c1de09377585a6db2cdd421e0e57ddf8a5992336
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-06-23 19:50:50 +0300

    adding entry animation and removal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 import NewChallenge from './NewChallenge.jsx';
+import { AnimatePresence } from 'motion/react';
 
 export default function Header() {
   const [isCreatingNewChallenge, setIsCreatingNewChallenge] = useState();
@@ -15,11 +16,13 @@ export default function Header() {
 
   return (
     <>
-      {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
+      <AnimatePresence>
+        {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
+      </AnimatePresence>
 
-      <header id="main-header">
+      <header id='main-header'>
         <h1>Your Challenges</h1>
-        <button onClick={handleStartAddNewChallenge} className="button">
+        <button onClick={handleStartAddNewChallenge} className='button'>
           Add Challenge
         </button>
       </header>
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,20 @@
 import { createPortal } from 'react-dom';
+import { motion } from 'motion/react';
 
 export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
-      <div className="backdrop" onClick={onClose} />
-      <dialog open className="modal">
+      <div className='backdrop' onClick={onClose} />
+      <motion.dialog
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 40 }}
+        open
+        className='modal'
+      >
         <h2>{title}</h2>
         {children}
-      </dialog>
+      </motion.dialog>
     </>,
     document.getElementById('modal')
   );
